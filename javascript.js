1f874f8bc5aaccd40179fcc25b50817596f49cba
Daniyal Ali <syeddaniyalali078@gmail.com>
2023-08-18 01:19:39 +0500

    first puch

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function searchButton() {
+    document.getElementById('search-bar').style.top = '0'
+}
+function searchButtonClose() {
+    document.getElementById('search-bar').style.top = '-300px'
+}
+
+
+function myFunction() {
+    document.getElementById('mobile-toggle-nav').style.left = '0'
+}
+function myClose() {
+    document.getElementById('mobile-toggle-nav').style.left = '-430px'
+}
